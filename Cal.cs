c815c05d83e217d474bc10bafcf023f1830f5761
“battusairam” <battusairam6@gmail.com>
2023-06-07 12:16:10 +0530

    [SAIRAM]: UnitTesting On Calculator

---
+++ Cal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Cal
+    {
+        public int addition(int x, int y)
+        {
+            return x+ y;
+        }
+        public int subtraction(int x, int y)
+        {
+            return x - y;
+        }
+        public int division(int x, int y)
+        {
+            return x / y;
+        }
+        public int multipication(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
